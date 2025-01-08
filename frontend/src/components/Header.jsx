13ff06c16de9da3e634e1ada2350ffdfc1e891b6
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2025-01-08 14:15:51 +0200

    creating search api route and update it on the client side

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../assets/images/IMG_6890.PNG";
-import { FaSearch } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FaSearch } from "react-icons/fa";
+import logo from "../assets/images/IMG_6890.PNG";
 
 const Logo = () => (
   <img src={logo} alt="Logo" className="h-8 sm:h-10 self-center" />
@@ -10,6 +10,24 @@ const Logo = () => (
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [window.location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -17,17 +35,32 @@ export default function Header() {
         <Link to="/">
           <Logo />
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button type="submit">
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
-          <li className="sm:inline text-slate-700 hover:underline">Home</li>
-          <li className="sm:inline text-slate-700 hover:underline">About</li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
           {currentUser ? (
             <Link to="/profile">
               <img
